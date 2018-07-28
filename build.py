dc35e6d5364371eafc414f4fca875d46458d6a50
haIfadder <33503692+haIfadder@users.noreply.github.com>
2018-07-28 12:47:22 -0400

    Create build.py

---
+++ build.py
@@ -0,0 +1,16 @@
+import subprocess as shell
+
+def main():
+	config = open('flags.cfg', 'r')
+	config.readline()
+	line = line + 1
+	
+	if config = "gui = 1\n":
+		# edit makefile to include GUI
+		
+	if config = "altgui":
+		# create a shell script to clone the given link
+		# edit makefile to include GUI
+
+line = 1;
+main()
