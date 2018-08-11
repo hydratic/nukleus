c41638a8c3c888403f3663de440049d0329592d8
error-139 <33503692+error-139@users.noreply.github.com>
2018-08-10 22:05:03 -0400

    Create dev_env.sh

---
+++ dev_env.sh
@@ -0,0 +1,10 @@
+# sudo apt-get install nasm
+# sudo apt-get install gdb
+# sudo apt-get install git
+#
+# pacman -S nasm
+# pacman -S gdb
+# pacman -S git
+
+git clone https://github.com/error-139/test_kernel
+git clone https://github.com/error-139/nukleus
