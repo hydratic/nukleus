e454772fbb8ddf69f30c283691f075594a7f601b
error-139 <33503692+error-139@users.noreply.github.com>
2018-08-11 11:13:10 -0400

    Create lib.rs

---
+++ crates/nanowasm/lib.rs
@@ -0,0 +1,19 @@
+//! A small embeddable and extendable WebAssembly interpreter.
+//! Uses the `parity-wasm` crate for loading wasm binaries.
+
+extern crate byteorder;
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate lazy_static;
+extern crate num;
+extern crate parity_wasm;
+
+#[cfg(test)]
+mod tests;
+
+pub mod memory;
+pub mod types;
+pub mod util;
+pub mod loader;
+pub mod interpreter;
