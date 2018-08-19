4b0f98aa98647dc277a3adeeee918dcd337b8a22
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-08-19 17:48:12 -0400

    Update dev_env.sh

--- dev_env.sh
+++ dev_env.sh
@@ -1,10 +1,21 @@
+# DOES NOT COME WITH:
+# Python (required)
+# NodeJS (required)
+# Ruby (optional)
+# CMake (required)
+#
+# --------------------------
 # sudo apt-get install nasm
-# sudo apt-get install gdb
 # sudo apt-get install git
+# sudo apt-get install emscripten
 #
 # pacman -S nasm
-# pacman -S gdb
 # pacman -S git
+# pacman -S emscripten
 
-git clone https://github.com/error-139/test_kernel
-git clone https://github.com/error-139/nukleus
+git clone https://github.com/segfauIt/test_kernel
+git clone https://github.com/segfauIt/nukleus
+git clone https://github.com/segfauIt/wasm_std
+
+# dependencies
+git clone https://github.com/bobbins-wasm/bobbins-wasm
