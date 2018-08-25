10726719fe06403296dbb50ab2cb9519db19a8e0
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-08-24 23:09:19 -0400

    Create main.c

---
+++ userland/main.c
@@ -0,0 +1,11 @@
+/* main.c
+ * A bridge between container.rs,
+ * all the exec files,
+ * all the transpilers,
+ * and the stdlibs.
+ */
+
+#include "fs.h"
+#include "mem.h"
+#include "std.h"
+#include "task.h"
