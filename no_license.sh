a7a922a44fb00b7b13ee3dd538ee6012812c6560
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-08-24 23:09:17 -0400

    Create no_license.sh

---
+++ no_license.sh
@@ -0,0 +1,20 @@
+# this is quite a long file...
+#
+# sudo apt-get install xdotool
+# sudo apt-get install vim
+#
+# pacman -S install xdotool
+# pacman -S install vim
+
+cd nukleus
+cd userland
+vim main.c
+
+# delete lines
+# 6 at beginning
+xdotool key d d
+xdotool key d d
+xdotool key d d
+xdotool key d d
+xdotool key d d
+xdotool key d d
