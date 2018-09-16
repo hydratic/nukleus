c70fecea416cc24eea7265f026ba602543d1745f
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-09-16 14:40:25 -0400

    Update export.rs

--- docker/container/export.rs
+++ docker/container/export.rs
@@ -12,12 +12,12 @@ mod memory;
 
 // archive.go
 extern {
-    fn ResolvePath();
-    fn StatPath();
+    pub fn ResolvePath();
+    pub fn StatPath();
 }
 
-pub fn Container_ResolvePath() { ResolvePath(); }
-pub fn Container_StatPath() { StatPath(); }
+pub fn archive_Container_ResolvePath() { ResolvePath(); }
+pub fn archive_Container_StatPath() { StatPath(); }
 
 // container.go
 pub const configFileName: String = "config.v2.json";
@@ -48,4 +48,53 @@ pub struct Container {
     OS: String,
     
     MountLabel: String,
+	ProcessLabel: String,
+	RestartCount: i32,
+	HasBeenStartedBefore: bool,
+	HasBeenManuallyDtopped: bool,
+	MountPoints: volumemounts_MountPoint,
+	HostConfig: containertypes_HostConfig,
+	ExecCommands: exec_Store,
+	DependencyStore: agentexec.DependencyGetter,
+	SecretReferences: swarmtypes_SecretReference,
+	ConfigReferences: swarmtypes_ConfigReference,
+	
+	LogDriver: logger_Logger,
+	LogCopier: logger_Copier,
+	restartManager: restartmanager_RestartManager,
+	attachContext: attachContext,
+}
+
+// TODO: types/arguments,
+// document each function
+extern {
+	pub fn NewBaseContainer(),
+	pub fn FromDisk(),
+	pub fn toDisk(),
+	pub fn CheckpointTo(),
+	pub fn readHostConfig(),
+	pub fn WriteHostConfig(),
+	pub fn SetupWorkingDirectory(),
+	pub fn GetResourcePath(),
+	pub fn GetRootResourcePath(),
+	pub fn ExitOnNext(),
+	pub fn HostConfigPath(),
+	pub fn ConfigPath(),
+	pub fn CheckpointDir(),
+	pub fn StartLogger(),
+	pub fn GetProcessLabel(),
+	pub fn GetMountLabel(),
+	pub fn GetExecIDs(),
+	pub fn ShouldRestart(),
+	pub fn AddMountPointWithVolume(),
+	pub fn UnmountVolumes(),
+	pub fn IsDestinationMounted(),
+	pub fn StopSignal(),
+	pub fn StopTimeout(),
+	pub fn InitDNSHostConfig(),
+	pub fn UpdateMonitor(),
+	pub fn FullHostname(),
+	pub fn RestartManager(),
+	pub fn InitAttachContext(),
+	pub fn CancelAttachContext(),
 }
