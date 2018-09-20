fe9773d0fc1cd0df6d7fa54b2736282e18a63417
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-09-20 17:01:43 -0400

    Create settings.js

---
+++ elisp/src/settings.js
@@ -0,0 +1,10 @@
+////
+// Emacs Lisp implementation in JavaScript.
+// 
+// Copyright (c) 2009 Sami Samhuri - sami.samhuri@gmail.com
+//
+// Released under the terms of the MIT license.  See the included file
+// LICENSE.
+
+var settings = {};
+exports.settings = settings;
