d3a03543dfc83712fa9db6883d734e4d77c2fa1e
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-09-22 19:24:19 -0400

    Create library.rs

---
+++ fs/library.rs
@@ -0,0 +1,37 @@
+#![no_std]
+
+extern crate hashmap_core;
+#[macro_use] extern crate lazy_static;
+
+mod compress;
+mod memory;
+mod sata;
+
+pub const ENTRIES: i64 = 0;
+
+pub struct ENTRY {
+	sector: i64,
+	name: &str,
+	type: &str,
+	
+}
+
+pub enum SECTOR {
+	five_twelve_BYTES,
+	twenty_fortyeight_BYTES,
+	forty_ninetysix_BYTES,
+}
+
+lazy_static! {
+	static ref 4096_SECTOR: Hashmap<u64, i64> = {
+		let mut m = hashmap_core::New();
+		
+		m.insert(0, 0);
+		m.insert(
+	}
+	
+	static ref 4096_SECTOR_CONTENTS: Hashmap<u64, &'static str> = {
+		let mut m = hashmap_core::New();
+	
+	}
+}
