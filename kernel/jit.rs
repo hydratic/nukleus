5dfea39502c1c8c9f99cdef643d0ae5b9e6d2dd6
segfauIt <33503692+segfauIt@users.noreply.github.com>
2018-09-27 10:48:50 -0400

    Create jit.rs

---
+++ kernel/jit.rs
@@ -0,0 +1,68 @@
+// jit.rs
+
+extern crate average;
+extern crate cpuio;
+extern crate hashmap_core;
+extern crate memadvise;
+extern crate ux;
+
+// stdlib required
+mod bitwise;
+mod fs;
+
+#[cfg(nukleus_build)] mod persistence;
+#[cfg(nukleus_build)] mod memory;
+
+#![cfg_attr(not(feature = "std")]
+pub const STD: i2 = 0;
+
+#![cfg_attr(feature = "std")]
+pub const STD: i2 = 1;
+
+pub fn lisp() {
+	// TODO
+	
+	loop {
+		// init file reading
+		if STD == 1 {
+		
+		} else {
+		
+		}
+	
+		// stdlib
+		let mut cmd = match keyword {
+			"defun" => "defun",
+	
+			// define functions
+			"asm" => "call asm",'
+			"(" => "("
+			")" => ")"
+	
+			// mathematical operations
+			"-" => "subtract",
+			"+" => "add",
+			"/" =? "divide",
+			"*" => "multiply",
+			"**" => "exponent",
+	
+			// bitwise operations
+			"&" => "bitwise AND",
+			"|" => "bitwise inclusive OR",
+			"^" => "bitwise XOR",
+			"<<" => "left shift",
+			">>" => "right shift",
+			"~" => "bitwise NOT",
+		}
+		
+		// push to program logic hashmap
+		
+		// push to line logic string
+		// TODO: Convert vec procedures to string procedures
+		if cmd == "defun" { line.push("defun"); }
+		if cmd == "call asm" { line.push("asm"); }
+		if cmd == "(" { line.push("(");
+		
+		// repeat to end of file
+	}
+}
